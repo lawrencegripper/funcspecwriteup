47e59852d987c619cc30002ab780790c355cebd4
Lawrence Gripper <info@grippers.co.uk>
2019-09-18 14:44:27 +0100

    Finished work on dotnet sample showing issue.

--- dotnetsample/Program.cs
+++ dotnetsample/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using Microsoft.Azure.Management.WebSites;
+using Microsoft.Rest;
+using SlavaGu.ConsoleAppLauncher;
 
 namespace dotnetsample
 {
@@ -6,7 +9,32 @@ namespace dotnetsample
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Starting");
+
+            var subscriptionId = ConsoleApp.Run("az", "account show --query id -o tsv").Output.Trim();
+            var token = ConsoleApp.Run("az", "account get-access-token --query accessToken -o tsv").Output.Trim();
+
+            Console.WriteLine($"Running with Sub: {subscriptionId}");
+            Console.WriteLine($"Running with Token: {token}");
+
+
+            var serviceCreds = new TokenCredentials(token, "Bearer");
+
+            var webClient = new WebSiteManagementClient(serviceCreds);
+            webClient.SubscriptionId = subscriptionId;
+
+            var functionSecrets = webClient.WebApps.ListFunctionSecrets("funcRestSpecTest", "testfuncx8b7a", "testfunc");
+
+            if (functionSecrets.Name == null)
+            {
+                Console.WriteLine("Due to spec issue #2 we see unpopulated Name properties");
+            }
+
+            if (functionSecrets.Key == null)
+            {
+                Console.WriteLine("Due to spec issue #1 we end up here");
+            }
+
         }
     }
 }
